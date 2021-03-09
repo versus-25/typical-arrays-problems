1c83b230dba46bf323167bf1114263fcbcf77438
versus-25 <ivan_schkola@mail.ru>
2021-03-09 13:29:14 +0300

    fix: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  array.sort((a, b) => {return a - b})
+  return array[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  array.sort((a, b) => {return b - a})
+  return array[-1]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  return  array.reduce((sum, item) => {sum + item})/array.length
 }
