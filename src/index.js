0b1f45ccf048d3386bd543a949c1ad1f138d3398
versus-25 <ivan_schkola@mail.ru>
2021-03-09 14:37:05 +0300

    fix: fix condition of empty array and svg func

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 
 exports.min = function min (array) {
-  if (!!array) return 0;
+  if (!array || array.length == 0) return 0;
   array.sort((a, b) => {return a - b})
   return array[0]
 }
 
 exports.max = function max (array) {
-  if (!!array) return 0;
+  if (!array || array.length == 0) return 0;
   array.sort((a, b) => {return b - a})
   return array[0]
 }
 
 exports.avg = function avg (array) {
-  if (!!array) return 0;
-  return  array.reduce((sum, item) => {sum + item})/array.length
+  if (!array || array.length == 0) return 0;
+  return array.reduce((sum, item) => sum + item)/array.length
 }
